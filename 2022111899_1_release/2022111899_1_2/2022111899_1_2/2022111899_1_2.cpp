8a1d1473a0cb13bef1cfca171d72699dd9d07afc
Ningyue Ji <79038651+738NGX@users.noreply.github.com>
2022-09-12 21:19:36 +0800

    Add files via upload

---
+++ 2022111899_1_release/2022111899_1_2/2022111899_1_2/2022111899_1_2.cpp
@@ -0,0 +1,8 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "1\n2 3\n4 5 6\n7 8 9 10" << endl;
+	return 0;
+}
---
+++ 2022111899_1_release/2022111899_1_4/2022111899_1_4/2022111899_1_4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num,a,b,c,d;
+    cout << "请输入一个四位整数：" << endl;
+    cin >> num;
+    a = num / 1000;
+    b = (num - 1000 * a) / 100;
+    c = (num - 1000 * a - 100 * b) / 10;
+    d = num % 10;
+    cout << num << "的千位数字是" << a << "，百位数字是" << b << "，十位数字是" << c << "，个位数字是" << d << endl;
+    return 0;
+}
